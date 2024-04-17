8a4f8ea7b33a58932c4477b81355825836f7c246
Cheryl Pappenheimer <cheryl.theroux@gmail.com>
2024-04-17 13:57:45 -0400

    Started docs.

---
+++ docs/source/index.rst
@@ -0,0 +1,22 @@
+.. Graphics 4-vecs documentation master file, created by
+   sphinx-quickstart on Fri Apr 12 10:58:01 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Graphics 4-vecs's documentation!
+===========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+TEST
+====
+TODO
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- src/graphics_4vecs/plot_4vecs.py
+++ src/graphics_4vecs/plot_4vecs.py
@@ -9,4 +9,42 @@ def hello() -> None:
     log.info("Hello from plot")
 
 
+def f(x: float) -> float:
+    """
+    TEST DOC GENERATION
+
+    One-line summary of function
+
+    Much longer and more detailed paragraph(s) about the function.  This is in
+    the NumPy style.
+
+    Parameters
+    ----------
+    x : float
+        This is a parameter. Notice the type - that predates Python 3!
+
+    Returns
+    -------
+    float
+        A description of what it returns.
+
+    Raises
+    ------
+    AssertionError
+        Explain what you can raise and why.
+
+
+    References
+    ----------
+    .. [1] "NumPy", https://numpy.org
+
+    Examples
+    --------
+    >>> y = f(1.0)
+    >>> y = f(2.0)
+    """
+
+    return x*2
+
+
 # TODO
