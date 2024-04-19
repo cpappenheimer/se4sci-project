96dc2708e0abe26fe0ffce2d72242b9cc4767072
Cheryl Pappenheimer <cheryl.theroux@gmail.com>
2024-04-19 13:16:20 -0400

    Docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,6 @@ author = "Cheryl Pappenheimer, Mitanshu Thakore, Lael Verace"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "myst_parser",
     "sphinx.ext.autodoc",
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
