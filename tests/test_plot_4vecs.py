43520dcbf3add9af1c4d12ed289178d7ce2b1f40
Cheryl Pappenheimer <cheryl.theroux@gmail.com>
2024-04-22 18:14:11 -0400

    Added skeleton test back.

--- tests/test_plot_4vecs.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-import logging
-
-log = logging.getLogger("graphics_4vecs")
--- tests/test_transformations_4vecs.py
+++ tests/test_transformations_4vecs.py
@@ -3,3 +3,10 @@ from __future__ import annotations
 import logging
 
 log = logging.getLogger("graphics_4vecs")
+
+
+def test_transformations_4vecs_hello():
+    log.info("Hello from test transformations")
+
+
+# TODO
